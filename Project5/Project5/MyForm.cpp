d8b157551e207fb447c95c92926a0626fc03ffb0
cxlevi <146369182+cxlevi@users.noreply.github.com>
2023-10-15 20:01:59 +0300

    Add files via upload

---
+++ Project5/Project5/MyForm.cpp
@@ -0,0 +1,13 @@
+#include "MyForm.h"
+
+using namespace System; // Указывает пространство имен для использования  
+using namespace System::Windows::Forms; // Указывает пространство имен для Windows Forms  
+
+[STAThreadAttribute] // позволяет работать с текстом  
+
+int main(array<String^>^ args) { // Точка входа в приложение, принимает массив строк в качестве аргументов  
+    Application::SetCompatibleTextRenderingDefault(false); // Устанавливает совместимость с отображением текста по умолчанию в значение false  
+    Application::EnableVisualStyles(); // Включает визуальные стили для приложения  
+    Project5::MyForm form; // Создает экземпляр класса MyForm из пространства имен Project5(Project5 - название проекта)  
+    Application::Run(% form); // Запускает приложение и запускает основной цикл обработки сообщений для указанной формы  
+}
---
+++ Project5/Project5/MyForm.h
@@ -0,0 +1,81 @@
+#pragma once
+
+namespace Project5 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::ListBox^ listBox1;
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->Location = System::Drawing::Point(35, 29);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(317, 316);
+			this->listBox1->TabIndex = 0;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(415, 400);
+			this->Controls->Add(this->listBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void groupBox1_Enter(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
