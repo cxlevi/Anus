d7540e5a096978af68654dd914f4924d158e8d85
DronkAnton <146369111+DronkAnton@users.noreply.github.com>
2023-10-16 19:25:32 +0300

    Update MyForm.cpp

--- Project5/Project5/MyForm.cpp
+++ Project5/Project5/MyForm.cpp
@@ -19,5 +19,58 @@ Project5::MyForm::MyForm(void)
 
 Project5::MyForm::MyForm(void) {
 
+    InitializeComponent();
 
+}
+
+ void::Project5::MyForm::textBox_Appear() {
+     //Создание нового экземпляра TextBox
+     TextBox^ newTextBox = gcnew TextBox();
+
+     // Назначение уникального имени TextBox
+     newTextBox->Name = "textBox" + textBoxIndex;
+     textBoxIndex++;
+
+     // Назначение позиции и размера TextBox
+     newTextBox->Location = System::Drawing::Point(10, 10 + (25 * textBoxIndex));
+     newTextBox->Size = System::Drawing::Size(100, 20);
+
+     // Добавление TextBox на панель
+     LB_Заметки->Controls->Add(newTextBox);
+}
+ void::Project5::MyForm::CheckBox_Appear() {
+
+     CheckBox^ newCheckBox = gcnew CheckBox();
+
+     newCheckBox->Name = "textBox" + CheckBoxIndex;
+     CheckBoxIndex++;
+
+     newCheckBox->Location = System::Drawing::Point(10, 10 + (25 * textBoxIndex));
+     newCheckBox->Size = System::Drawing::Size(100, 20);
+
+     LB_СписокДел->Controls->Add(newCheckBox);
+     
+ }
+
+
+ System::Void Project5::MyForm::выходToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+
+ }
+System::Void Project5::MyForm::менюToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+
+}
+System::Void Project5::MyForm::ЗаметкиToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+
+}
+ System::Void Project5::MyForm:: СписокДелToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+
+}
+System::Void Project5::MyForm::настройкиToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+
+}
+System::Void Project5::MyForm::btn_Заметки_Click(System::Object^ sender, System::EventArgs^ e) {
+    textBox_Appear();
+}
+System::Void Project5::MyForm::btn_СписокДел_Click(System::Object^ sender, System::EventArgs^ e) {
+    CheckBox_Appear();
 }
